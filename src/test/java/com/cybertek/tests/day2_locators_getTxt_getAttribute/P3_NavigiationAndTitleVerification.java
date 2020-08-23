9eee37b14775ed227792b9d82c7f1904414ddb43
proroksin <sinaverkemalov@gmail.com>
2020-08-23 14:09:09 -0500

    commit

--- src/test/java/com/cybertek/tests/day2_locators_getTxt_getAttribute/P3_NavigiationAndTitleVerification.java
+++ src/test/java/com/cybertek/tests/day2_locators_getTxt_getAttribute/P3_NavigiationAndTitleVerification.java
@@ -1,8 +1,53 @@
 package com.cybertek.tests.day2_locators_getTxt_getAttribute;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class P3_NavigiationAndTitleVerification {
 
     public static void main(String[] args) {
+
+        //TC #3: Back and forth navigation
+        //1- Open a chrome browser
+        WebDriverManager.chromedriver().setup();
+
+        //open browser
+        WebDriver driver = new ChromeDriver();
+
+        //2 - GO to: https://google.com
+        driver.get("https://google.com");
+
+        //3 - Click to Gmail from top right
+        driver.findElement(By.linkText("Gmail")).click();
+
+        //4-verify title contains
+        //Expected: Gmail
+        String expectedInTitle = "Gmail";
+        String actualTitle = driver.getTitle();
+
+        if (actualTitle.contains(expectedInTitle)){
+            System.out.println("Title verification PASSED!");
+        }else{
+            System.out.println("Title verification FAILED!!!");
+        }
+
+
+        //5-GO back to Google by using the .back();
+        driver.navigate().back();
+
+        // 6- Verify title equals:
+        //Expected: Google
+
+        String expectedTitle = "Google";
+        String actualGoogleTitle = driver.getTitle();
+
+        if(actualGoogleTitle.equals(expectedTitle)){
+            System.out.println("Google title verification PASSED!");
+        }else{
+            System.out.println("Google title verification FAILED!!!");
+        }
 
     }
 }
---
+++ src/test/java/com/cybertek/tests/day2_locators_getTxt_getAttribute/P4_GoogleSearch.java
@@ -0,0 +1,39 @@
+package com.cybertek.tests.day2_locators_getTxt_getAttribute;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class P4_GoogleSearch {
+
+    public static void main(String[] args) {
+
+        //TC #4: Google search
+        //1- Open a chrome browser
+        //setup a browser driver
+        WebDriverManager.chromedriver().setup();
+
+        //create driver instance and open chrome browser
+        WebDriver driver = new ChromeDriver();
+
+        //2- Go to: https://google.com
+        driver.get("https://google.com");
+
+        //3- Write “apple” in search box
+        // to be able to write anything in the search box
+        // i need to locate the search box and use sendKeys() method to send "apple"
+
+        driver.findElement(By.name("q")).sendKeys("apple"+ Keys.ENTER);
+
+        // 4- Click google search button
+
+        //5- Verify title:
+        //Expected: Title should start with “apple” word
+
+
+
+
+    }
+}
