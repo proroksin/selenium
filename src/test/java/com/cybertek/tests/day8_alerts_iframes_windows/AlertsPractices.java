a6e1bb860f7a2ccb3981aab636bc559d903690b7
proroksin <sinaverkemalov@gmail.com>
2020-09-27 13:56:25 -0500

    commit

--- src/test/java/com/cybertek/tests/day8_alerts_iframes_windows/AlertsPractices.java
+++ src/test/java/com/cybertek/tests/day8_alerts_iframes_windows/AlertsPractices.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -40,6 +41,35 @@ public class AlertsPractices {
 
         //2- Use "alert" instance to accept the javascript alert(popup)
         alert.accept();
+
+        //Locating the result text web element
+        WebElement resultText = driver.findElement(By.xpath("//p[@id='result']"));
+
+        //assert "resultText" is displayed
+        Assert.assertTrue(resultText.isDisplayed(), "Result text is not displayed. Verification failed!!!");
+
+    }
+
+    @Test
+    public void p2_confirmation_alert_practice(){
+        //Locating the warning/information alert button to click it
+        WebElement warningAlertButton = driver.findElement(By.xpath("//button[.='Click for JS Confirm']"));
+
+        //click to the button
+        warningAlertButton.click();
+
+        //Create alert instance
+        Alert alert = driver.switchTo().alert();
+
+        //We can either accept(), or dismiss() the confirmation alert
+        //alert.dismiss();
+        alert.accept();
+
+        //Locating the result text web element
+        WebElement resultText = driver.findElement(By.xpath("p[@id='result']"));
+
+        //Assert
+        Assert.assertTrue(resultText.isDisplayed(), "Text is not displayed. Verification FAILED!!!");
 
 
     }
