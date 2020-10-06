bd4f1783b452fe60de34373bdb9f6a047e41f655
proroksin <sinaverkemalov@gmail.com>
2020-10-06 18:18:20 -0500

    commit

---
+++ src/test/java/com/cybertek/tests/day10_testbase_properties_driverUtil/DriverUtilsPractice.java
@@ -0,0 +1,12 @@
+package com.cybertek.tests.day10_testbase_properties_driverUtil;
+
+import com.cybertek.utilities.ConfigurationReader;
+import com.cybertek.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DriverUtilsPractice {
+}
