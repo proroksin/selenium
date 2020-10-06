0e4bef110a7258b85746f0a2696cab6c9e46305b
proroksin <sinaverkemalov@gmail.com>
2020-10-06 18:19:15 -0500

    commit

--- src/test/java/com/cybertek/tests/day10_testbase_properties_driverUtil/DriverUtilsPractice.java
+++ src/test/java/com/cybertek/tests/day10_testbase_properties_driverUtil/DriverUtilsPractice.java
@@ -9,4 +9,24 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class DriverUtilsPractice {
+    @Test
+    public void simple_google_search_test(){
+        //Driver.getDriver() = driver
+        //From now on we will call the driver --> Driver.getDriver();
+        Driver.getDriver().get("https://google.com");
+
+        //locating the google search box using its name attribute value
+        WebElement searchBox = Driver.getDriver().findElement(By.name("q"));
+        String search = ConfigurationReader.getProperty("searchValue");
+
+        searchBox.sendKeys(search + Keys.ENTER);
+
+        //expected
+        String expectedInTitle = search;
+        //actual
+        String actualTitle = Driver.getDriver().getTitle();
+
+        Assert.assertTrue(actualTitle.contains(expectedInTitle));
+
+    }
 }
